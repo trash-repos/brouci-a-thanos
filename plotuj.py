4c6c304852e8378fa7806f37f66d41695374133c
strelda <strelecek.jan@seznam.cz>
2019-07-12 10:38:12 +0200

    final snad

--- plotuj.py
+++ plotuj.py
@@ -1,15 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from os import listdir
+from os.path import isfile, join
 
-with open("pocitej_output.txt") as f:
-    data = f.read().split(" ")
 
 
-matrix = np.loadtxt('pocitej_output.txt', usecols=range(4))
-mu= matrix[:,0]
-L = matrix[:,1]
-P = matrix[:,2]
-A = matrix[:,3]
+files = [fd for fd in ( join('data/', f) for f in listdir('data/') ) if isfile(fd)]
+
+mu_all = []
+A_all = []
+
+for f in files:
+    matrix = np.loadtxt(f, usecols=range(4))
+    mu= matrix[:,0]
+    L = matrix[:,1]
+    P = matrix[:,2]
+    A = matrix[:,3]
+
+    mu_all.extend(mu)
+    A_all.extend(A)
 
 
 fig = plt.figure()
@@ -20,8 +29,10 @@ ax1.set_title("Plot title...")
 ax1.set_xlabel('your x label..')
 ax1.set_ylabel('your y label...')
 
-ax1.scatter(mu, A, c='b', s=1, label='the data')
+#ax1.plot(mu, A, c='b', label='the data')
+
+ax1.scatter(mu_all, A_all, c='b', s=1, label='the data')
 
 leg = ax1.legend()
-
+plt.savefig("bifurcation.jpg", dpi=300)
 plt.show()
--- pocitej1D.py
+++ pocitej1D.py
@@ -2,10 +2,20 @@ import numpy as np
 import math
 import datetime
 from itertools import product
+from multiprocessing import Pool
+from os import listdir
+from os.path import isfile, join
+
+
+
+files = [fd for fd in ( join('data/', f) for f in listdir('data/') ) if isfile(fd)]
+for f in files:
+    os.remove(f)
+
 
 a = datetime.datetime.now()
 
-time = 100
+time = 200
 
 
 cla = 0.009
@@ -15,25 +25,42 @@ mul = 0.267
 mup = 0
 b = 7.48
 
-f = open('pocitej_output.txt', 'w')
 
-for mua in np.linspace(0.0005,0.010,500): #0.0035
-    L = 1
-    P = A = 0
 
-    for [mul, mup] in product(np.linspace(0.266,0.268,10), np.linspace(0,0.001,10)):
-        for t in range(0, time-1):
-            L2 = b*A*math.exp(-cla*A-cll*L)
-            P2 = L*(1-mul)
-            A2 = P*(1-mup)*math.exp(-cpa*A)+A*(1-mua)
+
+def vlakno(A):
+    with open('data/pocitej_output_' + str(A) + '.txt', 'w') as f:
+        for L in np.linspace(1,200,3):
+            for P in np.linspace(1,600,4):
+                for mua in np.linspace(0.0000,1,200000): #0.0035
+                    for t in range(0, time-1):
+                        L2 = b*A*np.exp(-cla*A-cll*L)
+                        P2 = L*(1-mul)
+                        A2 = P*(1-mup)*np.exp(-cpa*A)+A*(1-mua)
+                        
+                        L,P,A = L2,P2,A2
+                    f.write(f'{mua:.10f} {L:.5f} {P:.5f} {A:.5f} \n')
+        f.close()
+
+
+pool = Pool(8)
+sums = pool.map(vlakno, np.linspace(0,70,8))
+
+
+# for mua in np.linspace(0.0000,10,500): #0.0035
+#     L = 1
+#     P = A = 0
+
+#     for [mul, mup] in product(np.linspace(0.266,0.268,10), np.linspace(0,0.001,10)):
+#         for t in range(0, time-1):
+#             L2 = b*A*np.exp(-cla*A-cll*L)
+#             P2 = L*(1-mul)
+#             A2 = P*(1-mup)*np.exp(-cpa*A)+A*(1-mua)
             
-            L,P,A = L2,P2,A2
+#             L,P,A = L2,P2,A2
 
-        f.write(f'{mua:.10f} {L:.5f} {P:.5f} {A:.5f} \n')
+#         f.write(f'{mua:.10f} {L:.5f} {P:.5f} {A:.5f} \n')
 
 
-f.close()
-
-#np.savetxt('pocitej_output.txt', np.c_[L,P,A], fmt='%.0f')
 
 print(datetime.datetime.now()-a)
