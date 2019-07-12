39547398dd77ef9935d620d4fc15166a5a9bb305
m93a <m93a.cz@gmail.com>
2019-07-12 14:15:19 +0200

    něco to dělá, něco ne

--- plotuj.py
+++ plotuj.py
@@ -11,19 +11,15 @@ mu_all = []
 L_all = []
 P_all = []
 A_all = []
-L0_all = []
-P0_all = []
 A0_all = []
 
 for f in files:
-    matrix = np.loadtxt(f, usecols=range(7))
+    matrix = np.loadtxt(f, usecols=range(5))
     mu= matrix[:,0]
     L = matrix[:,1]
     P = matrix[:,2]
     A = matrix[:,3]
-    L0 = matrix[:,4]
-    P0 = matrix[:,5]
-    A0 = matrix[:,6]
+    A0 = matrix[:,4]
 
     print(A0[0])
 
@@ -31,8 +27,6 @@ for f in files:
     L_all.extend(L)
     P_all.extend(P)
     A_all.extend(A)
-    L0_all.extend(L0)
-    P0_all.extend(P0)
     A0_all.extend(A0)
 
 
@@ -43,30 +37,20 @@ total_all = [ L_all[i] + P_all[i] + A_all[i] for i in indexRange ]
 
 fig = plt.figure()
 
-# ax1 = fig.add_subplot(211)
+ax1 = fig.add_subplot(111)
 
-# ax1.set_title("Bifurkační diagram")
-# ax1.set_xlabel('mua')
-# ax1.set_ylabel('populace')
+ax1.set_title("Bifurkační diagram")
+ax1.set_xlabel('mua')
+ax1.set_ylabel('populace')
 
-# #ax1.plot(mu, A, c='b', label='the data')
+#ax1.plot(mu, A, c='b', label='the data')
 
-# ax1.scatter(mu_all, A_all, c='black', s=1, label='A')
-# ax1.scatter(mu_all, P_all, c='r', s=1, label='P')
-# ax1.scatter(mu_all, L_all, c='b', s=1, label='L')
-# ax1.scatter(mu_all, total_all, c='purple', s=1, label='total')
+ax1.scatter(mu_all, A_all, c='black', s=1, label='A')
+ax1.scatter(mu_all, P_all, c='r', s=1, label='P')
+ax1.scatter(mu_all, L_all, c='b', s=1, label='L')
+ax1.scatter(mu_all, total_all, c='purple', s=1, label='total')
 
-
-
-A_mu1  = [A_all[i]  for i in indexRange if mu_all[i] > .999]
-A0_mu1 = [A0_all[i] for i in indexRange if mu_all[i] > .999]
-
-ax2 = fig.add_subplot(111)
-ax2.set_title("Počáteční vs asymptotické A v mua > 0.995")
-ax2.set_xlabel("A0")
-ax2.set_ylabel("A inf")
-ax2.scatter(A0_mu1, A_mu1, s=1)
 
 leg = ax1.legend()
-plt.savefig("bifurcation.jpg", dpi=300)
+plt.savefig("bifurcation.png", dpi=300)
 plt.show()
--- pocitej1D.py
+++ pocitej1D.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 import datetime
-from itertools import product
+from itertools import product, chain
 from multiprocessing import Pool
 from os import listdir, remove
 from os.path import isfile, join
@@ -25,45 +25,33 @@ mul = 0.267
 mup = 0
 b = 7.48
 
+mua_ranges = chain(
+    np.linspace(0,   0.92, 500),
+    np.linspace(0.8, 0.9,  500),
+    np.linspace(0.9, 1,    10000),
+    np.linspace(0.1, 0.3,  1000)
+)
+
 
 def vlakno(A):
     A0 = A
+    L = 0
+    P = 0
     with open('data/pocitej_output_' + str(A0) + '.txt', 'w') as f:
-        for L in np.linspace(1,200,3):
-            L0 = L
-            for P in np.linspace(1,600,4):
-                P0 = P
-                for mua in np.linspace(0.98,1,50): #np.linspace(0.0000,1,200000): #0.0035
-                    for t in range(0, time-1):
-                        L2 = b*A*np.exp(-cla*A-cll*L)
-                        P2 = L*(1-mul)
-                        A2 = P*(1-mup)*np.exp(-cpa*A)+A*(1-mua)
-                        
-                        L,P,A = L2,P2,A2
-                    f.write(f'{mua:.10f} {L:.5f} {P:.5f} {A:.5f} {L0:.0f} {P0:.0f} {A0:.5f} \n')
+        for mua in mua_ranges: #np.linspace(0.0000,1,200000): #0.0035
+            for t in range(0, time):
+                L2 = b*A*np.exp(-cla*A-cll*L)
+                P2 = L*(1-mul)
+                A2 = P*(1-mup)*np.exp(-cpa*A)+A*(1-mua)
+                
+                L,P,A = L2,P2,A2
+            f.write(f'{mua:.10f} {L:.5f} {P:.5f} {A:.5f} {A0:.5f} \n')
         f.close()
 
 
 pool = Pool(8)
-sums = pool.map(vlakno, np.linspace(0,0.05,8))
+sums = pool.map(vlakno, np.linspace(0,70,8))
 
-# def vlakno(A):
-#     with open('data/pocitej_output_' + str(A) + '.txt', 'w') as f:
-#         for L in np.linspace(1,200,3):
-#             for P in np.linspace(1,600,4):
-#                 for mua in np.linspace(0.0000,1,200000): #0.0035
-#                     for t in range(0, time-1):
-#                         L2 = b*A*np.exp(-cla*A-cll*L)
-#                         P2 = L*(1-mul)
-#                         A2 = P*(1-mup)*np.exp(-cpa*A)+A*(1-mua)
-                        
-#                         L,P,A = L2,P2,A2
-#                     f.write(f'{mua:.10f} {L:.5f} {P:.5f} {A:.5f} \n')
-#         f.close()
-
-
-# pool = Pool(8)
-# sums = pool.map(vlakno, np.linspace(0,70,8))
 
 
 print(datetime.datetime.now()-a)
