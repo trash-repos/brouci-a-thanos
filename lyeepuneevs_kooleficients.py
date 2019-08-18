f97ad7c8f0afc86fb61f8509a81db85cbed2f9a5
DSroD <dsrod@seznam.cz>
2019-08-18 10:28:05 +0200

    ljapunovování asi funguje, juk na pipik.png

--- lyeepuneevs_kooleficients.py
+++ lyeepuneevs_kooleficients.py
@@ -1,5 +1,4 @@
 import numpy as np
-import datetime
 
 b = 7.48
 
@@ -10,7 +9,7 @@ def logistikuj(x, mua, mup, mul, cpa, cla, cll):
     A2 = x[1]*(1.-mup)*np.exp(-cpa*x[2])+x[2]*(1.-mua)
     return np.array([L2, P2, A2])
 
-d0 = np.array([0.,0.,10e-10])
+d0 = np.array([0.,0.,10e-12])
 x0 = np.array([100.,100.,100.])
 
 #mua0 = 0.2
@@ -21,20 +20,19 @@ cla0 = 0.009
 cll0 = 0.012
 cpa0 = 0.004
 
-for mua0 in np.linspace(0.9,1,100):
+for mua0 in np.linspace(0.8,1,10000):
     #Dojdi k atraktoru
     x1 = x0
-    for x in range(1000):
+    for x in range(500):
         x1 = logistikuj(x1, mua0, mup0, mul0, cpa0, cla0, cll0)
     xa = x1
     xb = x1 + d0
     sep = []
-    for i in range(10000):
+    for i in range(5000):
         xa = logistikuj(xa, mua0, mup0, mul0, cpa0, cla0, cll0)
         xb = logistikuj(xb, mua0, mup0, mul0, cpa0, cla0, cll0)
         d1 = xb-xa
-        dd = np.sqrt(np.dot(d1,d1)) / np.sqrt(np.dot(d0,d0))
-        if(i > 1000):
-            sep.append(np.log(np.abs(dd)))
+        dd = np.sqrt(np.dot(d1, d1) / np.dot(d0, d0))
+        sep.append(np.log(dd))
         xb = xa + (1./dd)*d1
     print(str(mua0) + " " + str(np.average(sep)))
--- plotuj_lye.py
+++ plotuj_lye.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('lyee.txt', sep=' ')
+data = pd.read_csv('sp.txt', sep=' ')
 data.columns = ['mua', 'l']
-plt.plot(data.mua, data.l)
-plt.plot([0.9+, 1], [0, 0])
-plt.savefig('pipik.png', dpi=1000)
+plt.plot(data.mua, data.l, linewidth=2)
+plt.plot([0, 1], [0, 0], linewidth=2)
+plt.axis([0.8, 1, -0.02, 0.02])
+plt.savefig('pipik.png', dpi=2000)
